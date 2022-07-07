ca4633464f1175ab8a9e09b84f7974b7ea6c3ae3
Gourav Khunger <gouravkhunger18@gmail.com>
2022-07-07 15:41:05 +0530

    chore: improvements

---
+++ assets/js/service-worker.js
@@ -0,0 +1,12 @@
+---
+permalink: /service-worker.js
+---
+
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.CacheFirst()
+);
