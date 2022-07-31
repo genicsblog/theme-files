49ae1731d383ba6137a0b2324d221397bcf5fac3
Gourav Khunger <gouravkhunger18@gmail.com>
2022-07-31 14:08:50 +0530

    fix(dev.sh): Add logic to detect changed dependency files

--- dev.sh
+++ dev.sh
@@ -3,32 +3,32 @@
 # This script removes the need for having docker to build the site locally.
 # You can run it through "./dev.sh"
 
-# move out of the .content folder once script is closed
-function cleanup () {
-  cd ..
-}
-
-trap "cleanup" 2
-
 if [ ! -d ".content" ]
 then
   echo "No cached '.content' directory found. Fetching the latest data..."
   git clone https://github.com/genicsblog/genicsblog.com .content -q
 fi
 
+DIFF_GEM=$(diff -q Gemfile .content/Gemfile)
+DIFF_PACKAGE=$(diff -q package.json .content/package.json)
+
 rsync -r --exclude '.content' . .content/
 cd .content
 
-if [ ! -d "node_modules" ]
+if [ ! -d ".bundle" ]; then bundle config set --local path 'vendor'; fi
+
+[ ! -z "$DIFF_GEM" ] && echo "Gemfile change detected!"
+[ ! -z "$DIFF_PACKAGE" ] && echo "package.json change detected!"
+
+if [ ! -d "node_modules" ] || [ ! -z "$DIFF_PACKAGE" ]
 then
   echo "Installing node dependencies..."
   npm i --silent
 fi
 
-if [ ! -d "vendor" ]
+if [ ! -d "vendor" ] || [ ! -z "$DIFF_GEM" ]
 then
   echo "Installing gem dependencies..."
-  bundle config set --local path 'vendor'
   bundle install --quiet
 fi
 
