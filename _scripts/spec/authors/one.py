2a7a77652fdb5027bb08d624b53936aa0f6e1d9f
Gourav Khunger <gouravkhunger18@gmail.com>
2022-11-04 12:52:49 +0530

    fix: case structure

--- _scripts/spec/authors/one.py
+++ _scripts/spec/authors/one.py
@@ -11,24 +11,7 @@ folder = "_scripts"
 script = "validate-authors.py"
 fail_count = 0
 
-# Case 1: Multiple files are edited along with _data/authors.yml
-command = f"""
-  echo 'Gemfile _data/authors.yml' >> temp.txt;
-  python3.9 {folder}/{script} gouravkhunger;
-"""
-res = subprocess.run(command, capture_output = True, shell = True)
-
-# The above case should pass if the command does not return 0
-if(res.returncode != 0):
-  print("Case 1: PASSED")
-else:
-  print("Case 1: FAILURE")
-  fail_count += 1
-
-# Reset temp.txt in between runs
-subprocess.run("rm temp.txt", shell = True)
-
-# Case 2: Only _data/authors.yml is edited.
+# Case 1: Only _data/authors.yml is edited.
 command = f"""
   echo '_data/authors.yml' >> temp.txt;
   python3.9 {folder}/{script} gouravkhunger;
@@ -37,6 +20,23 @@ res = subprocess.run(command, capture_output = True, shell = True)
 
 # The above case should pass if the command returns 0
 if(res.returncode == 0):
+  print("Case 1: PASSED")
+else:
+  print("Case 1: FAILURE")
+  fail_count += 1
+
+# Reset temp.txt in between runs
+subprocess.run("rm temp.txt", shell = True)
+
+# Case 2: Multiple files are edited along with _data/authors.yml
+command = f"""
+  echo 'Gemfile _data/authors.yml' >> temp.txt;
+  python3.9 {folder}/{script} gouravkhunger;
+"""
+res = subprocess.run(command, capture_output = True, shell = True)
+
+# The above case should pass if the command does not return 0
+if(res.returncode != 0):
   print("Case 2: PASSED")
 else:
   print("Case 2: FAILURE")
