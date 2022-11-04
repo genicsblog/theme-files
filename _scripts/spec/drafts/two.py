7b01a2a517e8f4faca6c285f46d39a2f8a15f548
Gourav Khunger <gouravkhunger18@gmail.com>
2022-11-04 14:15:43 +0530

    fix: failing tests

--- _scripts/spec/drafts/two.py
+++ _scripts/spec/drafts/two.py
@@ -28,24 +28,6 @@ else:
 # Reset temp.txt in between runs
 subprocess.run("rm temp.txt", shell = True)
 
-
-# Case 1: draft test.md is changed by committer gouravkhunger (VALID)
-command = f"""
-  echo '_drafts/test.md' >> temp.txt;
-  python3.9 {folder}/{script} gouravkhunger;
-"""
-res = subprocess.run(command, capture_output = True, shell = True)
-
-# The above case should pass if the command returns 0
-if(res.returncode == 0):
-  print("Case 1: PASSED")
-else:
-  print("Case 1: FAILED")
-  fail_count += 1
-
-# Reset temp.txt in between runs
-subprocess.run("rm temp.txt", shell = True)
-
 # Case 2: draft test.md is changed by committer kushagra (INVALID)
 command = f"""
   echo '_drafts/test.md' >> temp.txt;
@@ -54,14 +36,11 @@ command = f"""
 res = subprocess.run(command, capture_output = True, shell = True)
 
 # The above case should pass if the command does not return 0
-if(res.returncode == 0):
-  print("Case 1: PASSED")
+if(res.returncode != 0):
+  print("Case 2: PASSED")
 else:
-  print("Case 1: FAILED")
+  print("Case 2: FAILED")
   fail_count += 1
-
-# Reset temp.txt in between runs
-subprocess.run("rm temp.txt", shell = True)
 
 if fail_count != 0:
   exit(1)
